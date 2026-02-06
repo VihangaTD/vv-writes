9060b913e3864fafceba18ebd1595181267f4c39
Vihanga Tharuka <vihangatd99@gmail.com>
2026-02-07 00:12:31 +0530

    revalidate cached data

--- app/actions.ts
+++ app/actions.ts
@@ -6,6 +6,7 @@ import { fetchMutation } from "convex/nextjs";
 import { api } from "@/convex/_generated/api";
 import { redirect } from "next/navigation";
 import { getToken } from "@/lib/auth-server";
+import { revalidatePath } from "next/cache";
 
 export async function createBlogAction(values:z.infer<typeof  postSchema>){
 
@@ -55,5 +56,6 @@ export async function createBlogAction(values:z.infer<typeof  postSchema>){
         };
     }
 
+    revalidatePath("/blog");
     return redirect("/blog");
 }
--- app/api/create-blog/route.ts
+++ app/api/create-blog/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export default function POST(){
+export async function POST(){
     console.log('Hello from the server');
     return NextResponse.json({success:true});
 }
